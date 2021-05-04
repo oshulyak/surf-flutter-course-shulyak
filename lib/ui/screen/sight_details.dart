6cb21bd751073b491b6648990b0181c98189d476
oleg <oshulyak@gmail.com>
2021-05-04 10:39:41 +0600

    добавлены экраны sight_list, sight_details

---
+++ lib/ui/screen/sight_details.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+
+class SightDetails extends StatelessWidget {
+  final Sight sight;
+
+  SightDetails(this.sight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Image.asset(
+                sight.url,
+              ),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    padding: EdgeInsets.all(25),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Container(
+                        color: Colors.white,
+                        child: SizedBox(
+                          height: 40.0,
+                          width: 40.0,
+                          child: IconButton(
+                            color: Colors.white,
+                            padding: EdgeInsets.all(0),
+                            onPressed: () {
+                              print('BACK BUTTON CLICKED');
+                            },
+                            icon: Icon(
+                              Icons.navigate_before,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    sight.name,
+                    style: TextStyle(
+                        color: Color(0xFF3B3E5B),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 20),
+                        child: Text(
+                          sight.type,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'закрыто до 9:00',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Color(0xFF7C7E92)),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 18, top: 18),
+                  child: Text(
+                    sight.details,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton.icon(
+                      onPressed: null,
+                      icon: Icon(Icons.directions),
+                      label: Text(
+                        "Построить маршрут".toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                          // textStyle: TextStyle(fontSize: 30),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.green),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+
+                            // side: BorderSide(color: Colors.red)
+                          ))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton.icon(
+                onPressed: null,
+                icon: Icon(Icons.calendar_today),
+                label: Text(
+                  "Запланировать",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              FlatButton.icon(
+                onPressed: () => {print('to favorites')},
+                icon: Icon(Icons.favorite),
+                label: Text(
+                  "В Избранное",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:places/mocks.dart';
 import 'package:places/ui/screen/sight_card.dart';
+import 'package:places/ui/screen/sight_details.dart';
 
 class SightListScreen extends StatefulWidget {
   final String title = 'Places';
@@ -10,145 +11,36 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-     
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-          toolbarHeight: 100,
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          title: Text('Places'),
-          actions: []),
-//
-//
-//
-//
+        resizeToAvoidBottomInset: true,
+        // appBar: AppBar(
+        //     toolbarHeight: 100,
+        //     backgroundColor: Colors.transparent,
+        //     shadowColor: Colors.transparent,
+        //     title: Text('Список\nинтересных мест',
+        //         style: TextStyle(
+        //             color: Colors.black,
+        //             fontSize: 32,
+        //             fontWeight: FontWeight.bold)),
+        //     actions: []),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            SightDetails(mocks[2]),
 
-      body: Center(  
-          child: SightCard(
-            mocks[0],
-        // Sight(
-        //     name: 'Museum 1',
-        //     lat: 45.32323,
-        //     lon: 34.123423,
-        //     details: 'Descr 1',
-        //     type: 'museum'),
-      )),
-
-//
-//
-//
-//
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('Action'),
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Messages'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-    );
+            // SightCard(
+            //   mocks[0],
+            // ),
+            // SightCard(
+            //   mocks[1],
+            // ),
+            // SightCard(
+            //   mocks[0],
+            // ),
+            // SightCard(
+            //   mocks[1],
+            // ),
+          ],
+        )));
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-
-class SightListScreen extends StatefulWidget {
-  final String title = 'Places';
-  _SightListScreenState createState() => _SightListScreenState();
-}
-
-class _SightListScreenState extends State<SightListScreen> {
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      
-      appBar: AppBar(
-          toolbarHeight: 100,
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-
-          title: RichText(
-            textAlign: TextAlign.left,
-
-            text: TextSpan(
-                //text: 'Short Text',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 34,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'С',
-                    style: TextStyle(
-                      color: Colors.green,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'писок\n',
-                    style: TextStyle(
-                    ),
-                  ),  
-                  TextSpan(
-                    text: 'и',
-                    style: TextStyle(
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'нтересных мест',
-                    style: TextStyle(
-                    ),
-                  ),  
-                ]),
-          ),
-          actions: []),
-
-      body: Center(
-        child: Container(
-          color: Colors.yellow,
-          width: double.infinity,
-          height: 50,
-          child: RichText(
-            textAlign: TextAlign.end,
-            text: TextSpan(
-                text: 'Short Text',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'Text Rich',
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 20,
-                    ),
-                  ),
-                ]),
-            maxLines: 1,
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('Action'),
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Messages'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-    );
-  }
-}
-
-*/
